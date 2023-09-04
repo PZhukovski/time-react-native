7a79bd8475a64b45a3cfd5a3282c5e650354b489
Polina Zhukovskaya <mayabessonova_16_94@mail.ru>
2023-09-04 12:53:23 +0300

    feat: feating timefield component

--- App.tsx
+++ App.tsx
@@ -10,16 +10,32 @@ export default function App() {
             <Text>Привет! Ниже должно быть по больше примеров использования готового компонента. </Text>
             <Text>Разной ширины, с разным фоном и цветом шрифта</Text>
                 <FieldTime
-                    class={'mt-2 bg-primary'}
+                    class={'w-full h-12 mx-3.5 bg-primary text-mainText text-white'}
                     value={null}
                 />
                 <FieldTime
-                    class={'mt-2 bg-danger'}
+                    class={'max-w-xs h-8 mx-3.5p-2 bg-danger'}
                     value={new EDate().isoDate()}
                 />
                 <FieldTime
-                    value={null}
-                    class={'w-50 mt-2 bg-success'}
+                    value={"14:20"}
+                    class={'w-150 h-16 mx-3.5 bg-success'}
+                    useButtons={true}
+                />
+                 <FieldTime
+                    value={"14:20"}
+                    class={'w-max h-8 mx-3.5 bg-secondary text-body'}
+                    useButtons={true}
+                />
+                 <FieldTime
+                    value={"87:20"}
+                    class={'w-84 min-h-max mx-3.5 p-2 bg-silver text-successDark'}
+                    useButtons={true}
+                />
+                 <FieldTime
+                    value={"87:20"}
+                    class={'w-full min-h-fit mx-3.5 p-3 bg-warning text-mainText'}
+                    useButtons={true}
                 />
 
         </Col1>
--- src/RNCore/components/fields/FieldTime.tsx
+++ src/RNCore/components/fields/FieldTime.tsx
@@ -1,16 +1,85 @@
-import {TextInput} from "react-native";
+import React, { useEffect, useState } from "react";
+import { TextInput, View, TouchableOpacity } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 import tw from "../../libs/tailwind";
 
 interface ITimeField {
-    class?: string // tailwind classes
-    value: string | null // Если не задано отображаем --:--
-    onChange?: (time: string) => void // событие должно вызываться при снятии фокуса с компонента.
-    useButtons?: boolean // если true - отображаем кнопки "<" слева и справа ">" + или - 1 час соответственно
+  class?: string; // tailwind classes
+  value: string | null; // Если не задано, отображаем --:--
+  onChange?: (time: string) => void; // Событие должно вызываться при снятии фокуса с компонента
+  useButtons?: boolean; // Если true - отображаем кнопки "<" слева и справа ">" + или - 1 час соответственно
 }
 
 export function FieldTime(props: ITimeField) {
-    return <TextInput
-        style={tw`${props.class || ''}`}
-    />
+
+  const [formattedValue, setFormattedValue] = useState(props.value || "");
+
+  useEffect(() => {
+    if (props.value?.match(/^\d{2}:\d{2}$/)) {
+      const [hours, minutes] = (props.value || "").split(":").map(Number);
+      if (hours >= 0 && hours <= 23 && minutes >= 0 && minutes <= 59) {
+        setFormattedValue(props.value);
+      } else {
+        setFormattedValue("Вы ввели некорретное значение времени!");
+      }
+    } else {
+      setFormattedValue("--:--");
+    }
+  }, []);
+
+  const handleTextChange = (text: string) => {
+    const timeArray = text.split(":");
+    let hours = timeArray[0];
+    let minutes = timeArray[1];
+    hours = hours > "23" ? `0${hours[0]}` : hours;
+    minutes = minutes > "59" ? `00` : minutes;
+    const formattedTime = `${hours}:${minutes}`;
+    setFormattedValue(formattedTime);
+  };
+
+  const handleBlur = () => {
+    if (formattedValue.match(/^\d{2}:\d{2}$/)) {
+      if (props.onChange) {
+        props.onChange(formattedValue);
+      }
+    }
+  };
+
+  const handleHourChange = (increment: number) => () => {
+    const timeArray = formattedValue.split(":");
+    const currentHour = parseInt(timeArray[0], 10);
+    const newHour = (currentHour + increment + 24) % 24;
+    const newTime = `${newHour.toString()}:${timeArray[1]}`;
+    setFormattedValue(newTime);
+  };
+
+  return (
+    <View style={tw`flex flex-row mt-8 items-center`}>
+      {props.useButtons && (
+        <>
+          <TouchableOpacity onPress={handleHourChange(-1)}>
+            <FontAwesome name="minus" size={20} color="red" />
+          </TouchableOpacity>
+        </>
+      )}
+      <TextInput
+        value={formattedValue}
+        onChangeText={handleTextChange}
+        onBlur={handleBlur}
+        keyboardType="numeric"
+        maxLength={5}
+        style={tw`${props.class || ""}`}
+        multiline={true}
+        numberOfLines={undefined}
+      />
+      {props.useButtons && (
+        <>
+          <TouchableOpacity onPress={handleHourChange(1)}>
+            <FontAwesome name="plus" size={16} color="green" />
+          </TouchableOpacity>
+        </>
+      )}
+    </View>
+  );
 }
 
